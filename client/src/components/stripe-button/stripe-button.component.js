796022f5e14445b4f0bc868541e900c0f640a0e1
Alex <a.isaakiani@gmail.com>
2020-05-04 10:02:53 +0400

    update stripe payment to build comp;ete integration

--- client/src/components/stripe-button/stripe-button.component.js
+++ client/src/components/stripe-button/stripe-button.component.js
@@ -1,13 +1,27 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const pubblishableKey = "pk_test_Oyx2qGYPTnzJ6cWvcuG5CJ8Z00vELpTRhn";
 
   const onToken = (token) => {
-    console.log(token);
-    alert("Payment Successful");
+    axios({
+      url: "payment",
+      method: "post",
+      data: {
+        amount: priceForStripe,
+        token
+      }
+    })
+      .then((response) => {
+        alert("Payment successful");
+      })
+      .catch((error) => {
+        console.log('Payment error', JSON.parse(error));
+        alert("There was an issue with your payment. Please sure you use the provided credit card.");
+      });
   };
 
   return (
